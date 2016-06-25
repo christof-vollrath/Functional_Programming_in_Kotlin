99b8b039a440026fba0366edd54e58e37e30e733
Christof Vollrath <christof@taobits.net>
2016-06-25 20:13:00 +0200

    Balancing parentheses

---
+++ 1_Recursion/src/main/kotlin/Exercise2_BalancingParentheses.kt
@@ -0,0 +1,24 @@
+
+fun balance(str: String) = balance(str.toList())
+
+fun balance(chars: List<Char>): Boolean = balance(chars, 0)
+
+fun balance(chars: List<Char>, opened: Int): Boolean =
+        when {
+            chars.isEmpty() -> when {
+                opened == 0 -> true
+                else -> false
+            }
+            else -> {
+                when (chars.first()) {
+                    '(' -> balance(chars.tail(), opened+1)
+                    ')' -> when {
+                        opened > 0 -> balance(chars.tail(), opened-1)
+                        else -> false
+                    }
+                    else -> balance(chars.tail(), opened)
+                }
+            }
+        }
+
+fun <T> List<T>.tail(): List<T> = subList(1, size)
---
+++ 1_Recursion/src/test/kotlin/Exercise2_BalancingParenthesesTest.kt
@@ -0,0 +1,52 @@
+import org.junit.Test
+import org.junit.Assert.*
+
+class BalancingParenthesesTest {
+    @Test fun empty_string_should_be_balanced() {
+        assertTrue(balance(""))
+    }
+
+    @Test fun string_without_parentheses_should_be_balanced() {
+        assertTrue(balance("String example"))
+    }
+
+    @Test fun string_with_only_opening_parentheses_shouldnt_be_balanced() {
+        assertFalse(balance("("))
+    }
+
+    @Test fun string_with_only_opening_parentheses_and_other_chars_shouldnt_be_balanced() {
+        assertFalse(balance("a(b"))
+    }
+
+    @Test fun string_with_opening_and_closing_parentheses_should_be_balanced() {
+        assertTrue(balance("()"))
+    }
+
+    @Test fun string_with_opening_and_closing_parentheses_mixed_with_other_chars_should_be_balanced() {
+        assertTrue(balance("a(b)c"))
+    }
+
+    @Test fun nested_balanced_parentheses() {
+        assertTrue(balance("a(b(c)d)e"))
+    }
+
+    @Test fun nested_balanced_parentheses_which_are_not_balanced() {
+        assertFalse(balance("a(b(c)d"))
+    }
+
+    @Test fun example1() {
+        assertTrue(balance("(if (zero? x) max (/ 1 x))"))
+    }
+
+    @Test fun example2() {
+        assertTrue(balance("I told him (that it’s not (yet) done). (But he wasn’t listening)"))
+    }
+
+    @Test fun example3() {
+        assertFalse(balance(":-)"))
+    }
+
+    @Test fun example4() {
+        assertFalse(balance("())("))
+    }
+}
