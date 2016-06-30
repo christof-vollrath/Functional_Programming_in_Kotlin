b6431f59054895eaa7a5a852a0af3499cd89bf90
Christof Vollrath <christof@taobits.net>
2016-07-01 00:56:43 +0200

    Higher order functions - currying

--- 2_Higher_Order_Functions/src/main/kotlin/Lecture_Higher_Order_Functions.kt
+++ 2_Higher_Order_Functions/src/main/kotlin/Lecture_Higher_Order_Functions.kt
@@ -12,3 +12,53 @@ fun sumFactorials(a: Int, b: Int) = sum(::fact, a, b)
 fun id(x: Int): Int = x
 fun cube(x: Int): Int = x * x * x
 fun fact(x: Int): Int = if (x == 0) 1 else x * fact(x-1)
+
+// Using anonymous functions (closures)
+
+fun sumInts2(a: Int, b: Int) = sum({it}, a, b)
+fun sumCubes2(a: Int, b: Int) = sum({it*it*it}, a, b)
+// Not posible for fact, because this is a recursive function
+
+// Tail recursive sum
+
+fun sum3(f: (Int) -> Int, a: Int, b: Int): Int = sum3Loop(f, a, b, 0)
+
+private tailrec fun sum3Loop(f: (Int) -> Int, a: Int, b: Int, acc: Int): Int =
+    if (a > b) acc
+    else sum3Loop(f, a+1, b, acc+f(a))
+
+fun sumInts3(a: Int, b: Int) = sum3({it}, a, b)
+fun sumCubes3(a: Int, b: Int) = sum3({it*it*it}, a, b)
+fun sumFactorials3(a: Int, b: Int) = sum3(::fact, a, b)
+
+// Function as a result
+
+fun createAdd(): (Int, Int)-> Int = { a, b -> a + b }
+
+// Curried sum - Now sum returns a function which will iterate over the interval
+
+fun sumCurried(f: (Int) -> Int): (Int, Int) -> Int = { a, b -> sum(f, a, b) }
+
+val sumIntsCurried = sumCurried({ it })
+val sumCubesCurried = sumCurried({ it * it * it })
+val sumFactorialsCurried = sumCurried(::fact)
+
+// Curried product
+
+fun product(f: (Int) -> Int, a: Int, b: Int): Int =
+        if (a > b) 1
+        else f(a) * product(f, a+1, b)
+
+fun productCurried(f: (Int) -> Int): (Int, Int) -> Int = { a, b -> product(f, a, b) }
+
+// Factorial through product
+
+val factorialThroughProduct = { x: Int -> product({it}, 1, x)}
+
+// Generalized sum and product
+
+fun mapReduce(f: (Int) -> Int, combine: (Int, Int) -> Int, initial: Int, a: Int, b: Int): Int =
+        if (a > b) initial
+        else combine(f(a),  mapReduce(f, combine, initial, a+1, b))
+
+fun mapReduceCurried(f: (Int) -> Int, combine: (Int, Int) -> Int, initial: Int):  (Int, Int) -> Int = { a, b -> mapReduce(f, combine, initial, a, b) }
--- 2_Higher_Order_Functions/src/test/kotlin/Lecture_Higher_Order_FunctionsTest.kt
+++ 2_Higher_Order_Functions/src/test/kotlin/Lecture_Higher_Order_FunctionsTest.kt
@@ -7,16 +7,117 @@ class Lecture_Higher_Order_FunctionsTest {
 // Higher Order Functions
 //------------------------
 
-
-    @Test fun sumInts_1_2_3_should_be_6() {
+    @Test fun sumInts_1_3_should_be_6() {
         assertEquals(6, sumInts(1,3))
     }
 
-    @Test fun sumCubes_1_2_3_should_be_36() {
+    @Test fun sumCubes_1_3_should_be_36() {
         assertEquals(36, sumCubes(1,3))
     }
 
-    @Test fun sumFactorials_1_2_3_should_be_9() {
+    @Test fun sumFactorials_1_3_should_be_9() {
         assertEquals(9, sumFactorials(1,3))
     }
+
+    // Using anonymous functions (closures)
+
+    @Test fun sumInts2_1_3_should_be_6() {
+        assertEquals(6, sumInts2(1,3))
+    }
+
+    @Test fun sumCube2_1_3_should_be_36() {
+        assertEquals(36, sumCubes2(1,3))
+    }
+
+    // Tail recursive sum
+
+    @Test fun sumInts3_1_3_should_be_6() {
+        assertEquals(6, sumInts3(1,3))
+    }
+
+    @Test fun sumCubes3_1_3_should_be_36() {
+        assertEquals(36, sumCubes3(1,3))
+    }
+
+    @Test fun sumFactorials3_1_3_should_be_9() {
+        assertEquals(9, sumFactorials3(1,3))
+    }
+
+    // Function as result
+
+    @Test fun createAdd_1_2_should_be_3() {
+        assertEquals(3, createAdd()(1,2))
+    }
+
+    // Curried sum -- now sum returns a function which will iterate over the interval
+
+    @Test fun sumIntsCurried_1_3_should_be_6() {
+        assertEquals(6, sumIntsCurried(1,3))
+    }
+
+    @Test fun sumCubesCurried_1_3_should_be_36() {
+        assertEquals(36, sumCubesCurried(1,3))
+    }
+
+    @Test fun sumFactorialsCurried_1_3_should_be_9() {
+        assertEquals(9, sumFactorialsCurried(1,3))
+    }
+
+    // Curried sum - avoiding the middleman
+
+    @Test fun sumCurried_Ints_1_3_should_be_6() {
+        assertEquals(6, sumCurried { it } (1,3))
+    }
+
+    @Test fun sumCurried_Cubes_1_3_should_be_36() {
+        assertEquals(36, sumCurried { it*it*it} (1,3))
+    }
+
+    @Test fun sumCurried_Factorials_1_3_should_be_9() {
+        assertEquals(9, sumCurried(::fact)(1,3))
+    }
+
+    // Curried product
+
+    @Test fun productCurried_Ints_1_3_should_be_6() {
+        assertEquals(6, productCurried { it } (1,3))
+    }
+
+    @Test fun productCurried_Cubes_1_3_should_be_216() {
+        assertEquals(216, productCurried { it*it*it} (1,3))
+    }
+
+    @Test fun productCurried_Factorials_1_3_should_be_12() {
+        assertEquals(12, productCurried(::fact)(1,3))
+    }
+
+    @Test fun productCurried_Square_3_7_should_be_6350400() {
+        assertEquals(6350400,productCurried({it*it})(3, 7))
+    }
+
+    // Factorial through product
+
+    @Test fun factorialThroughProduct_5_should_be_120() {
+        assertEquals(120, factorialThroughProduct(5))
+    }
+
+
+    // Generalized sum and product
+
+    @Test fun mapReduce_sumCubes_1_3_should_be_36() {
+        assertEquals(36, mapReduce({it*it*it}, { a: Int, b: Int -> a + b}, 0, 1, 3))
+    }
+
+    @Test fun mapReduce_productFactorials_1_3_should_be_12() {
+        assertEquals(12, mapReduce(::fact, { a: Int, b: Int -> a * b}, 1, 1, 3))
+    }
+
+    @Test fun mapReduceCurried_sumCubes_1_3_should_be_36() {
+        assertEquals(36, mapReduceCurried({it*it*it}, { a: Int, b: Int -> a + b}, 0)( 1, 3))
+    }
+
+    @Test fun mapReduceCurried_productFactorials_1_3_should_be_12() {
+        assertEquals(12, mapReduceCurried(::fact, { a: Int, b: Int -> a * b}, 1)( 1, 3))
+    }
+
 }
