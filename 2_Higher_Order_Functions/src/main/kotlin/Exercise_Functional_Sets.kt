49afdfea366211a4582324b76c971f3b67c39a00
Christof Vollrath <christof@taobits.net>
2016-07-01 22:46:32 +0200

    Exercise functional sets

--- 2_Higher_Order_Functions/src/main/kotlin/Exercise_Functional_Sets.kt
+++ 2_Higher_Order_Functions/src/main/kotlin/Exercise_Functional_Sets.kt
@@ -3,13 +3,44 @@
 
 // Sets defined through functions
 
-// Kotlin doesn't have type aliases, therefore a class is needed
+// Kotlin doesn't have type aliases, therefore a data class is needed to hold the type
 
-class FunctionalSet { }
+data class FunctionalSet(val check: (Int) -> Boolean )
 
+val emptySet = FunctionalSet({false})
 
-fun contains(functionalSet: FunctionalSet, i: Int): Boolean = false
+fun contains(functionalSet: FunctionalSet, i: Int): Boolean = functionalSet.check(i)
 
+// 2.1 Basic Functions on Sets
 
-fun singletonSet(i: Int): FunctionalSet = FunctionalSet()
+fun singletonSet(i: Int): FunctionalSet = FunctionalSet({it == i})
+
+fun union(set1: FunctionalSet, set2: FunctionalSet): FunctionalSet = FunctionalSet({ contains(set1, it) || contains(set2, it) })
+
+fun intersect(set1: FunctionalSet, set2: FunctionalSet): FunctionalSet = FunctionalSet({ contains(set1, it) && contains(set2, it) })
+
+fun diff(set1: FunctionalSet, set2: FunctionalSet): FunctionalSet = FunctionalSet({ contains(set1, it) && ! contains(set2, it)})
+
+// 2.2 Queries and Transformations on Sets
+
+fun forall(set: FunctionalSet, function: (Int) -> Boolean): Boolean =
+    inforall(set, function, -1000, 1000)
+
+private fun inforall(set: FunctionalSet, function: (Int) -> Boolean, a: Int, b: Int): Boolean =
+    if (a > b) true
+    else
+        if (contains(set, a) && !function(a)) false
+        else inforall(set, function, a+1, b)
+
+fun exists(set: FunctionalSet, function: (Int) -> Boolean): Boolean = ! forall(set, {a: Int -> ! function(a) })
+
+fun map(set: FunctionalSet, function: (Int) -> Int): FunctionalSet =
+    inmap(set, function, -1000, 1000)
+
+fun inmap(set: FunctionalSet, function: (Int) -> Int, a: Int, b: Int): FunctionalSet =
+    if (a > b) emptySet
+    else
+        if (contains(set, a)) union(singletonSet(function(a)), inmap(set, function, a+1, b))
+        else inmap(set, function, a+1, b)
+
 
--- 2_Higher_Order_Functions/src/test/kotlin/Exercise_Functional_SetsTest.kt
+++ 2_Higher_Order_Functions/src/test/kotlin/Exercise_Functional_SetsTest.kt
@@ -6,11 +6,122 @@ class Exercise_Function_SetsTest {
     // Basic Functions on Sets
     //------------------------
 
-    @Test fun empty_set_should_contain_no_element() {
-        assertFalse(contains(FunctionalSet(), 1))
+    @Test fun emptySet_should_contain_no_element() {
+        assertFalse(contains(emptySet, 1))
     }
+
+// 2.1 Basic Functions on Sets
+
+    // singletonSet
 
     @Test fun set_should_contain_an_element() {
         assertTrue(contains(singletonSet(1), 1))
     }
+
+    @Test fun set_with_wrong_element() {
+        assertFalse(contains(singletonSet(2), 1))
+    }
+
+    // union
+    @Test fun union_with_emptySet_on_left_side_should_not_change_set() {
+        val unionSet = union(emptySet, singletonSet(1))
+        assertTrue(contains(unionSet, 1))
+        assertFalse(contains(unionSet, 2))
+    }
+
+    @Test fun union_with_emptySet_on_right_side_should_not_change_set() {
+        val unionSet = union(singletonSet(1), emptySet)
+        assertTrue(contains(unionSet, 1))
+        assertFalse(contains(unionSet, 2))
+    }
+
+    @Test fun union_should_contain_all_elements_of_both_sets() {
+        val set1 = singletonSet(1)
+        val set2 = singletonSet(2)
+        val unionSet = union(set1, set2)
+        assertTrue(contains(unionSet, 1))
+        assertTrue(contains(unionSet, 2))
+        assertFalse(contains(unionSet, 3))
+    }
+
+    // intersect
+    @Test fun intersect_with_emptySet_on_left_side_should_be_empty_set() {
+        val intersectSet = intersect(emptySet, singletonSet(1))
+        assertFalse(contains(intersectSet, 1))
+    }
+
+    @Test fun intersect_with_emptySet_on_right_side_should_be_empty_set() {
+        val intersectSet = intersect(singletonSet(1), emptySet)
+        assertFalse(contains(intersectSet, 1))
+    }
+
+    @Test fun intersect_should_contain_only_elements_in_both_sets() {
+        val set1 = union(singletonSet(1), singletonSet(2))
+        val set2 = union(singletonSet(2), singletonSet(3))
+        val intersectSet = intersect(set1, set2)
+        assertFalse(contains(intersectSet, 1))
+        assertTrue(contains(intersectSet, 2))
+        assertFalse(contains(intersectSet, 3))
+        assertFalse(contains(intersectSet, 4))
+    }
+
+    // diff
+    @Test fun diff_should_contain_only_elements_in_first_set_which_are_not_in_second() {
+        val set1 = union(singletonSet(1), singletonSet(2))
+        val set2 = union(singletonSet(2), singletonSet(3))
+        val intersectSet = diff(set1, set2)
+        assertTrue(contains(intersectSet, 1))
+        assertFalse(contains(intersectSet, 2))
+        assertFalse(contains(intersectSet, 3))
+        assertFalse(contains(intersectSet, 4))
+    }
+
+    // 2.2 Queries and Transformations on Sets
+
+    // forall
+    @Test fun forall_for_empty_set_should_return_true() {
+        assertTrue(forall(emptySet, {false}))
+    }
+
+    @Test fun forall_even_for_set_of_even_numbers_should_return_true() {
+        val set = union(union(singletonSet(2), singletonSet(4)), singletonSet(6))
+        assertTrue(forall(set, {it % 2 == 0}))
+    }
+
+    @Test fun forall_even_for_set_of_with_mixed_numbers_should_return_false() {
+        val set = union(union(singletonSet(2), singletonSet(3)), singletonSet(6))
+        assertFalse(forall(set, {it % 2 == 0}))
+    }
+
+    // exists
+    @Test fun exists_for_empty_set_should_return_false() {
+        assertFalse(exists(emptySet, {true}))
+    }
+
+    @Test fun exist_odd_for_set_of_even_numbers_should_return_false() {
+        val set = union(union(singletonSet(2), singletonSet(4)), singletonSet(6))
+        assertFalse(exists(set, {it % 2 != 0}))
+    }
+
+    @Test fun exist_odd_for_set_of_with_mixed_numbers_should_return_true() {
+        val set = union(union(singletonSet(2), singletonSet(3)), singletonSet(6))
+        assertTrue(exists(set, {it % 2 != 0}))
+    }
+
+    // map
+    @Test fun map_for_empty_set_should_return_empty_set() {
+        val mappedSet = map(emptySet, {it*it})
+        assertFalse(contains(mappedSet, 1))
+    }
+
+    @Test fun map_square_should_return_set_of_squares() {
+        val set = union(union(singletonSet(1), singletonSet(2)), singletonSet(3))
+        val mappedSet = map(set, {it*it})
+        assertTrue(contains(mappedSet, 1))
+        assertFalse(contains(mappedSet, 2))
+        assertTrue(contains(mappedSet, 4))
+        assertFalse(contains(mappedSet, 3))
+        assertTrue(contains(mappedSet, 9))
+    }
+
 }
