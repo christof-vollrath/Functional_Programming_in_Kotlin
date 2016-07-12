af9452f06b669b8ff00b843e19df5a00476ffcab
Christof Vollrath <christof@taobits.net>
2016-07-13 01:05:30 +0200

    Nat

--- 4_Types_and_Pattern_Matching/src/main/kotlin/Lecture_Types_and_Pattern_Matching.kt
+++ 4_Types_and_Pattern_Matching/src/main/kotlin/Lecture_Types_and_Pattern_Matching.kt
@@ -7,6 +7,7 @@ abstract class PureBoolean {
     fun not(): PureBoolean = ifThenElse(pureFalse, pureTrue)
     fun equals(p: PureBoolean): PureBoolean = ifThenElse(p, p.not())
     fun unequals(p: PureBoolean): PureBoolean = ifThenElse(p.not(), p)
+    fun less(p: PureBoolean): PureBoolean = ifThenElse(pureFalse, p)
 }
 
 object pureFalse: PureBoolean() {
@@ -17,3 +18,38 @@ object pureTrue: PureBoolean() {
     override fun<T> ifThenElse(thn: T, els: T) = thn
 }
 
+// Nat (simple variant of pure integers)
+
+abstract class Nat {
+    abstract fun isZero(): Boolean
+    abstract fun predecessor(): Nat
+    fun successor(): Nat = Succ(this)
+    abstract operator fun plus(x: Nat): Nat
+    abstract operator fun minus(x: Nat): Nat
+}
+
+object zero: Nat() {
+    override fun isZero() = true
+    override fun predecessor(): Nat = throw IllegalArgumentException()
+    override operator fun plus(x: Nat) = x
+    override operator fun minus(x: Nat) =
+        if (x.isZero()) zero
+        else throw IllegalArgumentException()
+}
+
+class Succ(val n: Nat): Nat() {
+    override fun isZero() = false
+    override fun predecessor(): Nat = n
+    override operator fun plus(x: Nat) = Succ(predecessor() + x)
+    override operator fun minus(x: Nat) =
+        if (x.isZero()) this
+        else n - x.predecessor()
+}
+
+fun Nat.toInt(): Int =
+    if (isZero()) 0
+    else 1 + predecessor().toInt()
+
+fun Int.toNat(): Nat =
+    if(this == 0) zero
+    else Succ((this-1).toNat())
--- 4_Types_and_Pattern_Matching/src/test/kotlin/Lecture_Types_and_Pattern_MatchingTest.kt
+++ 4_Types_and_Pattern_Matching/src/test/kotlin/Lecture_Types_and_Pattern_MatchingTest.kt
@@ -64,6 +64,7 @@ class TypesAndPatternMatchingTest {
     @Test fun equals_true_false_should_return_false() {
         assertEquals(pureFalse, pureTrue.equals(pureFalse))
     }
+
     // unequals
     @Test fun unequals_true_true_should_return_false() {
         assertEquals(pureFalse, pureTrue.unequals(pureTrue))
@@ -76,6 +77,100 @@ class TypesAndPatternMatchingTest {
     }
     @Test fun unequals_true_false_should_return_true() {
         assertEquals(pureTrue, pureFalse.unequals(pureTrue))
+    }
+
+    // less
+    @Test fun less_false_true_should_return_true() {
+        assertEquals(pureTrue, pureFalse.less(pureTrue))
+    }
+    @Test fun less_false_false_should_return_false() {
+        assertEquals(pureFalse, pureFalse.less(pureFalse))
+    }
+    @Test fun less_true_false_should_return_false() {
+        assertEquals(pureFalse, pureTrue.less(pureFalse))
+    }
+    @Test fun less_true_true_should_return_false() {
+        assertEquals(pureFalse, pureTrue.less(pureTrue))
+    }
+
+    // Nat (simple variant of pure integers)
+
+    // zero
+    @Test fun zero_should_be_zero() {
+        assertTrue(zero.isZero())
+    }
+    // Succ
+    @Test fun one_should_not_be_zero() {
+        assertFalse(Succ(zero).isZero())
+    }
+    @Test fun one_should_have_predecessor_zero() {
+        assertTrue(Succ(zero).predecessor().isZero())
+    }
+    // Helper function toInt
+    @Test fun zero_toInt_should_be_0() {
+        assertEquals(0, zero.toInt())
+    }
+    @Test fun one_toInt_should_be_1() {
+        assertEquals(1, Succ(zero).toInt())
+    }
+    @Test fun three_toInt_should_be_3() {
+        assertEquals(3, Succ(Succ(Succ(zero))).toInt())
+    }
+    // Helper function toNat
+    @Test fun toNat_0_should_be_zero() {
+        assertEquals(0, 0.toNat().toInt())
+    }
+    @Test fun toNat_1_should_be_one() {
+        assertEquals(1, 1.toNat().toInt())
+    }
+    @Test fun toNat_2_should_be_two() {
+        assertEquals(2, 2.toNat().toInt())
+    }
+    // predecessor
+    @Test(expected = IllegalArgumentException::class) fun zero_predecessor_should_throw_exception() {
+        zero.predecessor()
+    }
+    @Test fun one_predecessor_should_be_zero() {
+        assertEquals(0, 1.toNat().predecessor().toInt())
+    }
+    @Test fun ten_predecessor_should_be_nine() {
+        assertEquals(9, 10.toNat().predecessor().toInt())
+    }
+    // successor
+    @Test fun zero_successor_should_be_one() {
+        assertEquals(1, 0.toNat().successor().toInt())
+    }
+    @Test fun one_successor_should_be_two() {
+        assertEquals(2, 1.toNat().successor().toInt())
+    }
+    // plus
+    @Test fun zero_plus_zero_should_be_zero() {
+        assertEquals(0, ((0.toNat())+(0.toNat())).toInt())
+    }
+    @Test fun zero_plus_one_should_be_one() {
+        assertEquals(1, ((0.toNat())+(1.toNat())).toInt())
+    }
+    @Test fun one_plus_one_should_be_two() {
+        assertEquals(2, ((1.toNat())+(1.toNat())).toInt())
+    }
+    @Test fun ten_plus_five_should_be_fifteen() {
+        assertEquals(15, ((10.toNat())+(5.toNat())).toInt())
+    }
+    // minus
+    @Test fun zero_minus_zero_should_be_zero() {
+        assertEquals(0, ((0.toNat())-(0.toNat())).toInt())
+    }
+    @Test(expected = IllegalArgumentException::class)  fun zero_minus_one_should_throw_exception() {
+        ((0.toNat())-(1.toNat())).toInt()
+    }
+    @Test fun one_minus_zero_should_be_one() {
+        assertEquals(1, ((1.toNat())-(0.toNat())).toInt())
+    }
+    @Test fun one_minus_one_should_be_zero() {
+        assertEquals(0, ((1.toNat())-(1.toNat())).toInt())
+    }
+    @Test fun ten_minus_three_should_be_seven() {
+        assertEquals(7, ((10.toNat())-(3.toNat())).toInt())
     }
 }
 
