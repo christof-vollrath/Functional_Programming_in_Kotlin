453262246652d19bcf937333a19ff256d1bb1a9c
Christof Vollrath <christof@taobits.net>
2016-07-30 13:46:34 +0200

    Refactored using nested functions

--- 2_Higher_Order_Functions/src/main/kotlin/Exercise_Functional_Sets.kt
+++ 2_Higher_Order_Functions/src/main/kotlin/Exercise_Functional_Sets.kt
@@ -27,24 +27,25 @@ fun filter(set: FunctionalSet, filterFunct: (Int) -> Boolean): FunctionalSet = F
 
 // 2.2 Queries and Transformations on Sets
 
-fun forall(set: FunctionalSet, function: (Int) -> Boolean): Boolean =
-    inforall(set, function, -1000, 1000) // Here, we consider that an integer x has the property -1000 <= x <= 1000 in order to limit the search space
-
-private fun inforall(set: FunctionalSet, function: (Int) -> Boolean, a: Int, b: Int): Boolean =
-    if (a > b) true
-    else
-        if (contains(set, a) && !function(a)) false
-        else inforall(set, function, a+1, b)
+fun forall(set: FunctionalSet, function: (Int) -> Boolean): Boolean {
+    fun inforall(a: Int, b: Int): Boolean =
+            if (a > b) true
+            else
+                if (contains(set, a) && !function(a)) false
+                else inforall(a+1, b)
+    return inforall(-1000, 1000) // Here, we consider that an integer x has the property -1000 <= x <= 1000 in order to limit the search space
+}
 
 fun exists(set: FunctionalSet, function: (Int) -> Boolean): Boolean = ! forall(set, {a: Int -> ! function(a) })
 
-fun map(set: FunctionalSet, function: (Int) -> Int): FunctionalSet =
-    inmap(set, function, -1000, 1000)
+fun map(set: FunctionalSet, function: (Int) -> Int): FunctionalSet {
+    fun inmap(a: Int, b: Int): FunctionalSet =
+            if (a > b) emptySet
+            else
+                if (contains(set, a)) union(singletonSet(function(a)), inmap(a + 1, b))
+                else inmap(a + 1, b)
+    return inmap(-1000, 1000)
+}
 
-fun inmap(set: FunctionalSet, function: (Int) -> Int, a: Int, b: Int): FunctionalSet =
-    if (a > b) emptySet
-    else
-        if (contains(set, a)) union(singletonSet(function(a)), inmap(set, function, a+1, b))
-        else inmap(set, function, a+1, b)
 
 
